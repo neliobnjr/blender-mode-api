2693152eb834dba9726da15cb325eec28da79099
Nelio Batista <nelio@electricfish.co>
2023-10-23 03:46:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,23 +7,23 @@ app = Flask(__name__)
 theta = 0.0
 a = 0.5
 b = 0.2
-delta_theta = 0.1  # incremento em theta para cada atualização
+delta_theta = 0.1 
 
 @app.route('/position', methods=['GET'])
 def position():
     global theta
-    # Calcular a posição r baseado no ângulo atual
+    
     r = a + b * theta
-    # Converter coordenadas polares em cartesianas
+    
     x = r * math.cos(theta)
     y = r * math.sin(theta)
-    # Incrementar o ângulo para a próxima chamada
+    
     theta += delta_theta
 
-    # Aqui, a posição em z é mantida constante, mas você pode modificá-la se quiser movimento vertical
+    
     z = 0.0
 
-    # Retornar posição atual
+    
     return jsonify({"x": x, "y": y, "z": z}), 200
 
 if __name__ == '__main__':
